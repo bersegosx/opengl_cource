cab4c65f06f5f93b555650c2ba5f5ea9c3f7403a
mashihara <bersegosx@gmail.com>
2019-12-29 00:15:35 +0500

    [Lesson 7] Shader uniform var

--- OpenGL_course/main.cpp
+++ OpenGL_course/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 // GLEW - OpenGL Extenstion Wrangler, OpenGL interface
 #define GLEW_STATIC
@@ -18,15 +19,23 @@
 // Window dimension
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformXMove;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.0005f;
 
 // Vertex Shader
 static const char* vShader = "                      \n\
 #version 330                                        \n\
                                                     \n\
 layout (location = 0) in vec3 pos;                  \n\
-void main() {                                        \n\
-    gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);   \n\
+                                                    \n\
+uniform float xMove;                                \n\
+                                                    \n\
+void main() {                                       \n\
+    gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);   \n\
 }                                                  \n\
 ";
 
@@ -115,6 +124,8 @@ void CompileShaders() {
         printf("Error validating programm: %s\n", elog);
         return;
     }
+    
+    uniformXMove = glGetUniformLocation(shader, "xMove");
 }
 
 int main(int argc, const char * argv[]) {
@@ -167,11 +178,23 @@ int main(int argc, const char * argv[]) {
         // handle user events
         glfwPollEvents();
         
+        if (direction) {
+            triOffset += triIncrement;
+        } else {
+            triOffset -= triIncrement;
+        }
+        
+        if (abs(triOffset) >= triMaxOffset) {
+            direction = !direction;
+        }
+        
         // clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         glUseProgram(shader);
+        
+        glUniform1f(uniformXMove, triOffset);
         
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
